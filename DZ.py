df06b12b76f60530be37dcfae9935db50795a129
Olga Mikhailova <Olyaya-l@yandex.ru>
2022-09-21 20:52:17 +0300

    task #3

--- DZ.py
+++ DZ.py
@@ -21,9 +21,22 @@ print(sum) """
 
 - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4) """
 
-N = int(input('Введите число N '))
+""" N = int(input('Введите число N '))
 numbers = [i for i in range(1, N+1)]
 print(numbers)
 for i in range(1,N):
     numbers[i] = numbers[i] * numbers[i-1]
-print(numbers)
+print(numbers) """
+
+"""
+Задача 3.
+Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+"""
+
+n = int(input('Введите n '))
+sum = 0
+for i in range(1, n+1): 
+    m = ((1 + ((1 / i)-int(1/i)))**i)
+    print(m)
+    sum = m + sum
+print(f'Сумма чисел последовательности $(1+\frac 1 n)^n$ =', {sum})  
